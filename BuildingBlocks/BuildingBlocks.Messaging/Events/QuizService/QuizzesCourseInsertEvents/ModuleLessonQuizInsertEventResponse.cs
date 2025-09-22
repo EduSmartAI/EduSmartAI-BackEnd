37eeee74d96009b894da4965bfc7968600297d5e
Thanh_An <vuthanhan3547@gmail.com>
2025-09-23 01:49:29 +0700

    [Add][BuildingBlocks] QuizzesCourseInsertEvents

---
+++ BuildingBlocks/BuildingBlocks.Messaging/Events/QuizService/QuizzesCourseInsertEvents/ModuleLessonQuizInsertEventResponse.cs
@@ -0,0 +1,18 @@
+﻿using BaseService.Common.ApiEntities;
+
+namespace BuildingBlocks.Messaging.Events.QuizService.QuizzesCourseInsertEvents
+{
+	public record ModuleLessonQuizInsertEventResponse : AbstractApiResponse<ModuleLessonQuizInsertEventResponseEntity>
+	{
+		public override ModuleLessonQuizInsertEventResponseEntity Response { get; set; }
+	}
+
+
+	public record ModuleLessonQuizInsertEventResponseEntity(
+		Guid CorrelationId,
+		Guid QuizId,
+		Guid? ModuleId,
+		Guid? LessonId
+	);
+
+}
