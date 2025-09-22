4562c412ba4c3d02374b528ff380baaacb45c28f
Thanh_An <vuthanhan3547@gmail.com>
2025-09-22 17:00:53 +0700

    [Add][CourseService] Init 2 api get course by slug for guest and lecture

--- Services/CourseService/Course.Application/Interfaces/ICourseService.cs
+++ Services/CourseService/Course.Application/Interfaces/ICourseService.cs
@@ -4,6 +4,7 @@ using Course.Application.Courses.Commands.UpdateCourse;
 using Course.Application.Courses.Commands.UpdateCourseModules;
 using Course.Application.Courses.Queries.CheckEnrollment;
 using Course.Application.Courses.Queries.GetCourseById;
+using Course.Application.Courses.Queries.GetCourseBySlug;
 using Course.Application.Courses.Queries.GetCourses;
 using Course.Application.Courses.Queries.GetCourseTags;
 using Course.Application.DTOs.CoursesDTO;
@@ -21,7 +22,9 @@ namespace Course.Application.Interfaces
 		Task<UpdateCourseModulesResponse> UpdateCourseModulesAsync(Guid courseId, UpdateCourseModulesDto dto, CancellationToken ct = default);
 
 		Task<GetCourseByIdForGuestResponse> GetCourseByIdForGuestAsync(Guid Id, CancellationToken ct = default);
+		Task<GetCourseBySlugForGuestResponse> GetCourseBySlugForGuestAsync(string Slug, CancellationToken ct = default);
 		Task<GetCourseByIdForLectureResponse> GetCourseByIdForLectureAsync(Guid Id, CancellationToken ct = default);
+		Task<GetCourseBySlugForLectureResponse> GetCourseBySlugForLectureAsync(string Slug, CancellationToken ct = default);
 		
 		Task<CheckEnrollmentResponse> CheckEnrollmentAsync(Guid courseId, CancellationToken ct = default);
 		Task<GetCourseTagsResponse> GetCourseTagsAsync(CancellationToken ct = default);
