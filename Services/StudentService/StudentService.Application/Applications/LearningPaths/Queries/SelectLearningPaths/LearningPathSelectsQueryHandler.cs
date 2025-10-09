999c5b57baeb392bf878ec40a436a9c2d3f9ce0e
Andy150104 <tranduyanh7766@gmail.com>
2025-10-09 17:01:59 +0700

    [FIX][BE] Fix validation

--- Services/StudentService/StudentService.Application/Applications/LearningPaths/Queries/SelectLearningPaths/LearningPathSelectsQueryHandler.cs
+++ Services/StudentService/StudentService.Application/Applications/LearningPaths/Queries/SelectLearningPaths/LearningPathSelectsQueryHandler.cs
@@ -1,12 +1,10 @@
 ﻿using BuildingBlocks.CQRS;
-using BuildingBlocks.Messaging.Events.StudentService.GetInfoInternalCourse;
-using MassTransit;
 using StudentService.Application.Applications.LearningPaths.Queries.SelectLearningPaths;
 using StudentService.Application.Interfaces;
 
 namespace StudentService.Application.Applications.LearningPaths.Queries
 {
-    public class LearningPathSelectsQueryHandler(ILearningPathService _learningPathService, IRequestClient<GetInfoInternalCourseEvents> requestClient) : IQueryHandler<LearningPathSelectsQuery, LearningPathSelectResponse>
+    public class LearningPathSelectsQueryHandler(ILearningPathService _learningPathService) : IQueryHandler<LearningPathSelectsQuery, LearningPathSelectResponse>
     {
         public async Task<LearningPathSelectResponse> Handle(LearningPathSelectsQuery request, CancellationToken cancellationToken)
         {
