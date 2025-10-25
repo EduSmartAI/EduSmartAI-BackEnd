86f99833727d1290fb13b2fc0e113dded4627fb0
giakoii <khoipgse172073@fpt.edu.vn>
2025-10-25 15:59:34 +0700

    [Add][TeacherService] Add model class of TeacherService

---
+++ Services/TeacherService/TeacherService.Domain/WriteModels/TeacherQualification.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeacherService.Domain.WriteModels;
+
+public partial class TeacherQualification
+{
+    public Guid QualificationId { get; set; }
+
+    public Guid TeacherId { get; set; }
+
+    public string? DegreeTitle { get; set; }
+
+    public string? Institution { get; set; }
+
+    public DateOnly? StartDate { get; set; }
+
+    public DateOnly? EndDate { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? CertificateUrl { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public string? UpdatedBy { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public virtual Teacher Teacher { get; set; } = null!;
+}
