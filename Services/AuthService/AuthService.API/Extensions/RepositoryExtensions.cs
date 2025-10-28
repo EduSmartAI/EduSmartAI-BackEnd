0744c99e234266d156afb1f8936f5a6e5e697890
giakoii <khoipgse172073@fpt.edu.vn>
2025-10-28 23:12:51 +0700

    [Mod][AuthService] Modify api login

--- Services/AuthService/AuthService.API/Extensions/RepositoryExtensions.cs
+++ Services/AuthService/AuthService.API/Extensions/RepositoryExtensions.cs
@@ -22,6 +22,7 @@ public static class RepositoryExtensions
         services.AddScoped<ICommandRepository<Account>, CommandRepository<Account>>();
         services.AddScoped<ICommandRepository<Role>, CommandRepository<Role>>();
         services.AddScoped<ICommandRepository<OutboxMessage>, CommandRepository<OutboxMessage>>();
+        services.AddScoped<ICommandRepository<AdminAccount>, CommandRepository<AdminAccount>>();
         services.AddScoped<IQueryRepository<AccountCollection>, QueryRepository<AccountCollection>>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         
--- Services/AuthService/AuthService.Infrastructure/Context/AuthServiceContext.cs
+++ Services/AuthService/AuthService.Infrastructure/Context/AuthServiceContext.cs
@@ -12,6 +12,8 @@ public class AuthServiceContext(DbContextOptions<AuthServiceContext> options) : 
     
     public virtual DbSet<OutboxMessage> OutboxMessages { get; set; }
     
+    public virtual DbSet<AdminAccount> AdminAccounts { get; set; }
+    
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
@@ -43,6 +45,50 @@ public class AuthServiceContext(DbContextOptions<AuthServiceContext> options) : 
             entity.HasKey(x => x.Id);
             entity.Property(x => x.Name).HasMaxLength(256).IsRequired();
             entity.Property(x => x.NormalizedName).HasMaxLength(256).IsRequired();
+        });
+        
+        builder.Entity<AdminAccount>(entity =>
+        {
+            entity.ToTable("AdminAccounts");
+            
+            entity.HasKey(x => x.AccountId);
+            
+            entity.Property(x => x.FullName)
+                .HasMaxLength(256)
+                .IsRequired();
+            
+            entity.Property(x => x.PhoneNumber)
+                .HasMaxLength(20);
+            
+            entity.Property(x => x.Position);
+            
+            entity.Property(x => x.Note);
+            
+            entity.Property(x => x.IsSuperAdmin)
+                .HasDefaultValue(false)
+                .IsRequired();
+            
+            entity.Property(x => x.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .IsRequired();
+            
+            entity.Property(x => x.CreatedBy)
+                .HasMaxLength(256)
+                .IsRequired();
+            
+            entity.Property(x => x.UpdatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .IsRequired();
+            
+            entity.Property(x => x.UpdatedBy)
+                .HasMaxLength(256)
+                .IsRequired();
+            
+            entity.HasOne(a => a.Account)
+                .WithOne()
+                .HasForeignKey<AdminAccount>(a => a.AccountId)
+                .OnDelete(DeleteBehavior.Cascade);
+            entity.Property(x => x.IsActive).HasDefaultValue(true);
         });
         
         builder.Entity<OutboxMessage>(entity =>
