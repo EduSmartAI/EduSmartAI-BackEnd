3f9394b72c40be8689e2abfdbf21b52766f3ed94
giakoii <khoipgse172073@fpt.edu.vn>
2025-12-05 17:46:18 +0700

    no message

--- Services/AuthService/AuthService.Application/Accounts/Commands/ForgotPassword/ForgotPasswordCommand.cs
+++ Services/AuthService/AuthService.Application/Accounts/Commands/ForgotPassword/ForgotPasswordCommand.cs
@@ -1,6 +1,7 @@
 ﻿using BuildingBlocks.CQRS;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 namespace AuthService.Application.Accounts.Commands.ForgotPassword;
 public class ForgotPasswordCommand : ICommand<ForgotPasswordResponse>
 {
-    
+    [Required(ErrorMessage = "Email is required")]
+    public string Email { get; init; } = null!;
 }
 
--- Services/AuthService/AuthService.Application/Accounts/Commands/ForgotPassword/ForgotPasswordCommandHandler.cs
+++ Services/AuthService/AuthService.Application/Accounts/Commands/ForgotPassword/ForgotPasswordCommandHandler.cs
@@ -1,14 +1,43 @@
 ﻿using AuthService.Domain.WriteModels;
+using BaseService.Application.Interfaces.Commons;
 using BaseService.Application.Interfaces.Repositories;
+using BaseService.Common.Utils.Const;
 using BuildingBlocks.CQRS;
 
 namespace AuthService.Application.Accounts.Commands.ForgotPassword
 {
-    public class ForgotPasswordCommandHandler(ICommandRepository<Account> accountRepository) : ICommandHandler<ForgotPasswordCommand, ForgotPasswordResponse>
+    public class ForgotPasswordCommandHandler(ICommandRepository<Account> accountRepository, ICommonLogic commonLogic, IUnitOfWork unitOfWork) : ICommandHandler<ForgotPasswordCommand, ForgotPasswordResponse>
     {
-        public Task<ForgotPasswordResponse> Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
+        public async Task<ForgotPasswordResponse> Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var response = new ForgotPasswordResponse { Success = false };
+            var account = await accountRepository
+                .FirstOrDefaultAsync(
+                predicate: a => a.Email == request.Email && a.IsActive,
+                cancellationToken: cancellationToken);
+            if (account == null)
+            {
+                response.SetMessage(MessageId.E00000, "Email không tồn tại trong hệ thống");
+                return response;
+            }
+
+            string key = $"{DateTime.Now}-{account.Email}";
+
+            // Endrypt key send to email
+            var encryptedKey = commonLogic.EncryptText(key);
+
+            // Publish event to UtilityService to send mail
+            
+
+            // Save change
+            account.Key = encryptedKey.Response.EncryptedKey;
+            accountRepository.Update(account);
+            await unitOfWork.SaveChangesAsync(account.Email, cancellationToken);
+
+            // True
+            response.Success = true;
+            response.SetMessage(MessageId.I00001, "Đã gửi link xác nhận đến email của bạn");
+            return response;
         }
     }
 }
