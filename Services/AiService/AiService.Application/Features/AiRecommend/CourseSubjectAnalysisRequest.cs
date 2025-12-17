4885c08c64b56ddc8c080804e437be29fe134416
Andy150104 <tranduyanh7766@gmail.com>
2025-12-17 11:08:52 +0700

    [Add][BE] Add recommend improvement in learning course

---
+++ Services/AiService/AiService.Application/Features/AiRecommend/CourseSubjectAnalysisRequest.cs
@@ -0,0 +1,10 @@
+using MediatR;
+
+namespace AiService.Application.Features.AiRecommend
+{
+    public class CourseSubjectAnalysisRequest : IRequest<SubjectAnalysisResponse>
+    {
+        public required Guid CourseId { get; set; }
+    }
+}
+
